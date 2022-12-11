98cde5f956f0c77c08cab1c3c321f34b8d05b497
thebear8 <45793990+thebear8@users.noreply.github.com>
2022-12-11 16:13:41 +0100

    Add constructors for IR Nodes Explicit constructors are needed, because designated initialization does not work with non-aggregate types.

--- flat-v6/data/ir.hpp
+++ flat-v6/data/ir.hpp
@@ -31,7 +31,10 @@ struct IRStatement : public IRNode
 
 struct IRExpression : public IRNode
 {
-	Type* type = nullptr;
+	Type* type;
+
+	IRExpression() :
+		type(nullptr) {}
 
 	IMPLEMENT_ACCEPT()
 };
@@ -42,12 +45,18 @@ struct IRIntegerExpression : public IRExpression
 {
 	std::string value;
 
+	IRIntegerExpression(std::string const& value) :
+		value(value) {}
+
 	IMPLEMENT_ACCEPT()
 };
 
 struct IRBoolExpression : public IRExpression
 {
 	bool value;
+
+	IRBoolExpression(bool value) :
+		value(value) {}
 
 	IMPLEMENT_ACCEPT()
 };
@@ -56,6 +65,9 @@ struct IRCharExpression : public IRExpression
 {
 	uint32_t value;
 
+	IRCharExpression(uint32_t value) :
+		value(value) {}
+
 	IMPLEMENT_ACCEPT()
 };
 
@@ -63,12 +75,18 @@ struct IRStringExpression : public IRExpression
 {
 	std::string value;
 
+	IRStringExpression(std::string const& value) :
+		value(value) {}
+
 	IMPLEMENT_ACCEPT()
 };
 
 struct IRIdentifierExpression : public IRExpression
 {
 	std::string value;
+
+	IRIdentifierExpression(std::string const& value) :
+		value(value) {}
 
 	IMPLEMENT_ACCEPT()
 };
@@ -78,6 +96,9 @@ struct IRStructExpression : public IRExpression
 	std::string structName;
 	std::vector<std::pair<std::string, IRExpression*>> fields;
 
+	IRStructExpression(std::string const& structName, std::vector<std::pair<std::string, IRExpression*>> const& fields) :
+		structName(structName), fields(fields) {}
+
 	IMPLEMENT_ACCEPT()
 };
 
@@ -85,6 +106,9 @@ struct IRUnaryExpression : public IRExpression
 {
 	UnaryOperator operation;
 	IRExpression* expression;
+
+	IRUnaryExpression(UnaryOperator operation, IRExpression* expression) :
+		operation(operation), expression(expression) {}
 
 	IMPLEMENT_ACCEPT()
 };
@@ -94,6 +118,9 @@ struct IRBinaryExpression : public IRExpression
 	BinaryOperator operation;
 	IRExpression* left, * right;
 
+	IRBinaryExpression(BinaryOperator operation, IRExpression* left, IRExpression* right) : 
+		operation(operation), left(left), right(right) {}
+
 	IMPLEMENT_ACCEPT()
 };
 
@@ -101,6 +128,9 @@ struct IRCallExpression : public IRExpression
 {
 	IRExpression* expression;
 	std::vector<IRExpression*> args;
+
+	IRCallExpression(IRExpression* expression, std::vector<IRExpression*> const& args) :
+		expression(expression), args(args) {}
 
 	IMPLEMENT_ACCEPT()
 };
@@ -110,6 +140,9 @@ struct IRIndexExpression : public IRExpression
 	IRExpression* expression;
 	std::vector<IRExpression*> args;
 
+	IRIndexExpression(IRExpression* expression, std::vector<IRExpression*> const& args) :
+		expression(expression), args(args) {}
+
 	IMPLEMENT_ACCEPT()
 };
 
@@ -117,6 +150,9 @@ struct IRFieldExpression : public IRExpression
 {
 	IRExpression* expression;
 	std::string fieldName;
+
+	IRFieldExpression(IRExpression* expression, std::string const& fieldName) :
+		expression(expression), fieldName(fieldName) {}
 
 	IMPLEMENT_ACCEPT()
 };
@@ -127,12 +163,18 @@ struct IRBlockStatement : public IRStatement
 {
 	std::vector<IRStatement*> statements;
 
+	IRBlockStatement(std::vector<IRStatement*> const& statements) :
+		statements(statements) {}
+
 	IMPLEMENT_ACCEPT()
 };
 
 struct IRExpressionStatement : public IRStatement
 {
 	IRExpression* expression;
+
+	IRExpressionStatement(IRExpression* expression) :
+		expression(expression) {}
 
 	IMPLEMENT_ACCEPT()
 };
@@ -141,12 +183,18 @@ struct IRVariableStatement : public IRStatement
 {
 	std::vector<std::pair<std::string, IRExpression*>> items;
 
+	IRVariableStatement(std::vector<std::pair<std::string, IRExpression*>> const& items) :
+		items(items) {}
+
 	IMPLEMENT_ACCEPT()
 };
 
 struct IRReturnStatement : public IRStatement
 {
 	IRExpression* expression;
+
+	IRReturnStatement(IRExpression* expression) :
+		expression(expression) {}
 
 	IMPLEMENT_ACCEPT()
 };
@@ -156,6 +204,9 @@ struct IRWhileStatement : public IRStatement
 	IRExpression* condition;
 	IRStatement* body;
 
+	IRWhileStatement(IRExpression* condition, IRStatement* body) :
+		condition(condition), body(body) {}
+
 	IMPLEMENT_ACCEPT()
 };
 
@@ -163,6 +214,9 @@ struct IRIfStatement : public IRStatement
 {
 	IRExpression* condition;
 	IRStatement* body;
+
+	IRIfStatement(IRExpression* condition, IRStatement* body) :
+		condition(condition), body(body) {}
 
 	IMPLEMENT_ACCEPT()
 };
@@ -174,6 +228,9 @@ struct IRStructDeclaration : public IRDeclaration
 	std::string name;
 	std::vector<std::pair<std::string, Type*>> fields;
 
+	IRStructDeclaration(std::string const& name, std::vector<std::pair<std::string, Type*>> const& fields) :
+		name(name), fields(fields) {}
+
 	IMPLEMENT_ACCEPT()
 };
 
@@ -183,6 +240,9 @@ struct IRFunctionDeclaration : public IRDeclaration
 	Type* result;
 	std::vector<std::pair<std::string, Type*>> params;
 	IRStatement* body;
+
+	IRFunctionDeclaration(std::string const& name, Type* result, std::vector<std::pair<std::string, Type*>> const& params, IRStatement* body) :
+		name(name), result(result), params(params), body(body) {}
 
 	IMPLEMENT_ACCEPT()
 };
@@ -194,6 +254,9 @@ struct IRExternFunctionDeclaration : public IRDeclaration
 	Type* result;
 	std::vector<std::pair<std::string, Type*>> params;
 
+	IRExternFunctionDeclaration(std::string const& lib, std::string const& name, Type* result, std::vector<std::pair<std::string, Type*>> const& params) :
+		lib(lib), name(name), result(result), params(params) {}
+
 	IMPLEMENT_ACCEPT()
 };
 
@@ -204,4 +267,9 @@ struct IRSourceFile : public IRNode
 	std::vector<std::string> path;
 	std::vector<std::vector<std::string>> imports;
 	std::vector<IRDeclaration*> declarations;
+
+	IRSourceFile(std::vector<std::string> const& path, std::vector<std::vector<std::string>> const& imports, std::vector<IRDeclaration*> declarations) :
+		path(path), imports(imports), declarations() {}
+
+	IMPLEMENT_ACCEPT()
 };
