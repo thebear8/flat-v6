a63cac58dee6db0f04e8f30ab40ff7da3e2358d9
thebear8 <45793990+thebear8@users.noreply.github.com>
2023-03-01 13:36:09 +0100

    Update zip_view to https://github.com/thebear8/cpp20-ranges-zip/commit/32bea0a9dc34ac5d5a729cef17972ab153b22d24

--- flat-v6/util/zip_view.hpp
+++ flat-v6/util/zip_view.hpp
@@ -6,60 +6,59 @@ template<std::ranges::input_range Rng1, std::ranges::input_range Rng2>
     requires std::ranges::view<Rng1> && std::ranges::view<Rng2>
 struct zip_view : std::ranges::view_interface<zip_view<Rng1, Rng2>>
 {
-    using reference = std::pair<
-        std::ranges::range_reference_t<Rng1>,
-        std::ranges::range_reference_t<Rng2>>;
-    using const_reference = std::pair<
-        std::ranges::range_reference_t<Rng1>,
-        std::ranges::range_reference_t<Rng2>>;
+    template<typename TRange>
+    using range_iterator_t = decltype(std::begin(std::declval<TRange&>()));
+    template<typename TRange>
+    using range_reference_t = decltype(*std::begin(std::declval<TRange&>()));
+    template<typename TRange>
+    using range_const_reference_t =
+        decltype(*std::cbegin(std::declval<TRange&>()));
+
+    using reference =
+        std::pair<range_reference_t<Rng1>, range_reference_t<Rng2>>;
+    using const_reference =
+        std::pair<range_const_reference_t<Rng1>, range_const_reference_t<Rng2>>;
     using value_type = std::pair<
-        std::remove_cv_t<
-            std::remove_reference_t<std::ranges::range_reference_t<Rng1>>>,
-        std::remove_cv_t<
-            std::remove_reference_t<std::ranges::range_reference_t<Rng2>>>>;
+        std::remove_cvref_t<range_reference_t<Rng1>>,
+        std::remove_cvref_t<range_reference_t<Rng2>>>;
 
     struct iterator;
-    using const_iterator = iterator;
+    struct const_iterator;
 
 private:
-    std::ranges::iterator_t<Rng1> m_rng1_begin, m_rng1_end;
-    std::ranges::iterator_t<Rng2> m_rng2_begin, m_rng2_end;
+    Rng1 m_rng1;
+    Rng2 m_rng2;
 
 public:
     zip_view() = default;
     zip_view(Rng1&& rng1, Rng2&& rng2)
-        : m_rng1_begin(std::begin(rng1)),
-          m_rng1_end(std::end(rng1)),
-          m_rng2_begin(std::begin(rng2)),
-          m_rng2_end(std::end(rng2))
+        : m_rng1(std::move(rng1)), m_rng2(std::move(rng2))
     {
     }
 
-    zip_view(Rng1 const& rng1, Rng2 const& rng2)
-        : m_rng1_begin(std::begin(rng1)),
-          m_rng1_end(std::end(rng1)),
-          m_rng2_begin(std::begin(rng2)),
-          m_rng2_end(std::end(rng2))
-    {
-    }
+    zip_view(Rng1 const& rng1, Rng2 const& rng2) : m_rng1(rng1), m_rng2(rng2) {}
 
-    iterator begin() const { return iterator(m_rng1_begin, m_rng2_begin); }
-    iterator end() const { return iterator(m_rng1_end, m_rng2_end); }
+    iterator begin() const
+    {
+        return iterator(std::begin(m_rng1), std::begin(m_rng2));
+    }
+    iterator end() const
+    {
+        return iterator(std::end(m_rng1), std::end(m_rng2));
+    }
 
     const_iterator cbegin() const
     {
-        return const_iterator(m_rng1_begin, m_rng2_begin);
+        return const_iterator(std::cbegin(m_rng1), std::cbegin(m_rng2));
     }
     const_iterator cend() const
     {
-        return const_iterator(m_rng1_end, m_rng2_end);
+        return const_iterator(std::cbegin(m_rng1), std::cbegin(m_rng2));
     }
 };
 
 template<typename Rng1, typename Rng2>
-zip_view(
-    Rng1&& rng1, Rng2&& rng2
-) -> zip_view<std::ranges::views::all_t<Rng1>, std::ranges::views::all_t<Rng2>>;
+zip_view(Rng1 const&, Rng2 const&) -> zip_view<Rng1, Rng2>;
 
 template<std::ranges::input_range Rng1, std::ranges::input_range Rng2>
     requires std::ranges::view<Rng1> && std::ranges::view<Rng2>
@@ -87,7 +86,7 @@ public:
         return (*this);
     }
 
-    iterator operator++(int) { return ++iterator(m_it1, m_it2); }
+    iterator operator++(int) const { return ++iterator(m_it1, m_it2); }
 
     bool operator==(iterator const& other) const
     {
@@ -97,34 +96,68 @@ public:
         return (m_it1 == other.m_it1) || (m_it2 == other.m_it2);
     }
 
-    bool operator!=(iterator const& other) const
+    reference operator*() const { return reference(*m_it1, *m_it2); }
+    reference operator->() const { return reference(*m_it1, *m_it2); }
+};
+
+template<std::ranges::input_range Rng1, std::ranges::input_range Rng2>
+    requires std::ranges::view<Rng1> && std::ranges::view<Rng2>
+struct zip_view<Rng1, Rng2>::const_iterator
+{
+    using difference_type = ptrdiff_t;
+    using value_type = zip_view<Rng1, Rng2>::value_type;
+
+private:
+    std::ranges::iterator_t<Rng1> m_it1;
+    std::ranges::iterator_t<Rng2> m_it2;
+
+public:
+    const_iterator() = default;
+    const_iterator(
+        std::ranges::iterator_t<Rng1> it1, std::ranges::iterator_t<Rng2> it2
+    )
+        : m_it1(std::move(it1)), m_it2(std::move(it2))
     {
-        // check if both of the iterators are unqequal so we don't iterate past
-        // the end in case (m_it1 == end) and (m_it2 != end) or (m_it1 != end)
-        // and (m_it2 == end)
-        return (m_it1 != other.m_it1) && (m_it2 != other.m_it2);
     }
 
-    reference operator*() { return reference(*m_it1, *m_it2); }
+    const_iterator& operator++()
+    {
+        ++m_it1, ++m_it2;
+        return (*this);
+    }
+
+    const_iterator operator++(int) const
+    {
+        return ++const_iterator(m_it1, m_it2);
+    }
+
+    bool operator==(const_iterator const& other) const
+    {
+        // check if one of the iterators is equal so we don't iterate past the
+        // end in case (m_it1 == end) and (m_it2 != end) or (m_it1 != end) and
+        // (m_it2 == end)
+        return (m_it1 == other.m_it1) || (m_it2 == other.m_it2);
+    }
 
     const_reference operator*() const
     {
         return const_reference(*m_it1, *m_it2);
     }
 
-    reference operator->() { return reference(*m_it1, *m_it2); }
-
-    const_reference operator->() const { return reference(*m_it1, *m_it2); }
+    const_reference operator->() const
+    {
+        return const_reference(*m_it1, *m_it2);
+    }
 };
 
-static_assert((bool)std::input_iterator<zip_view<
-                  std::views::all_t<std::vector<int>>,
-                  std::views::all_t<std::vector<int>>>::iterator>);
+static_assert(std::input_iterator<zip_view<
+                  std::ranges::ref_view<std::vector<int>>,
+                  std::ranges::ref_view<std::vector<int>>>::iterator>);
 
-static_assert((bool)std::ranges::input_range<zip_view<
-                  std::views::all_t<std::vector<int>>,
-                  std::views::all_t<std::vector<int>>>>);
+static_assert(std::ranges::input_range<zip_view<
+                  std::ranges::ref_view<std::vector<int>>,
+                  std::ranges::ref_view<std::vector<int>>>>);
 
-static_assert((bool)std::ranges::view<zip_view<
-                  std::views::all_t<std::vector<int>>,
-                  std::views::all_t<std::vector<int>>>>);
+static_assert(std::ranges::view<zip_view<
+                  std::ranges::ref_view<std::vector<int>>,
+                  std::ranges::ref_view<std::vector<int>>>>);
