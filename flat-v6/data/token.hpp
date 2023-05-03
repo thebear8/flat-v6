10786f129b723f5c023cb1022070810aabe018a9
thebear8 <45793990+thebear8@users.noreply.github.com>
2023-05-03 20:22:45 +0000

    Update token.hpp

--- flat-v6/data/token.hpp
+++ flat-v6/data/token.hpp
@@ -1,11 +1,17 @@
 #pragma once
+#include <initializer_list>
+#include <string_view>
+
 #include "enum.hpp"
 
 #define TOKEN_LIST(ENTRY) \
-    ENTRY(Eof)            \
+    ENTRY(Invalid)        \
     ENTRY(Error)          \
+    ENTRY(Eof)            \
+    ENTRY(Space)          \
+    ENTRY(Tab)            \
     ENTRY(NewLine)        \
-    ENTRY(Whitespace)     \
+    ENTRY(Comment)        \
     ENTRY(Dot)            \
     ENTRY(Comma)          \
     ENTRY(Colon)          \
@@ -53,4 +59,39 @@
     ENTRY(Identifier)     \
     ENTRY(CharLiteral)    \
     ENTRY(StringLiteral)
+
 DEFINE_ENUM(Token, TOKEN_LIST)
+
+namespace TokenDefinitions
+{
+constexpr std::pair<std::string_view, Token> keywords[] = {
+    { "true", Token::True },     { "false", Token::False },
+    { "let", Token::Let },       { "return", Token::Return },
+    { "while", Token::While },   { "if", Token::If },
+    { "else", Token::Else },     { "struct", Token::Struct },
+    { "fn", Token::Function },   { "constraint", Token::Constraint },
+    { "import", Token::Import }, { "module", Token::Module },
+    { "fn", Token::Function },   { "extern", Token::Extern },
+};
+
+constexpr std::pair<std::string_view, Token> tokens[] = {
+    { ".", Token::Dot },          { ",", Token::Comma },
+    { ":", Token::Colon },
+
+    { "(", Token::ParenOpen },    { ")", Token::ParenClose },
+    { "[", Token::BracketOpen },  { "]", Token::BracketClose },
+    { "{", Token::BraceOpen },    { "}", Token::BraceClose },
+
+    { "+", Token::Plus },         { "-", Token::Minus },
+    { "!", Token::LogicalNot },   { "~", Token::BitwiseNot },
+    { "*", Token::Multiply },     { "/", Token::Divide },
+    { "%", Token::Modulo },       { "<<", Token::ShiftLeft },
+    { ">>", Token::ShiftRight },  { "&", Token::BitwiseAnd },
+    { "|", Token::BitwiseOr },    { "^", Token::BitwiseXor },
+    { "==", Token::Equal },       { "!=", Token::NotEqual },
+    { "<", Token::LessThan },     { ">", Token::GreaterThan },
+    { "<=", Token::LessOrEqual }, { ">=", Token::GreaterOrEqual },
+    { "&&", Token::LogicalAnd },  { "||", Token::LogicalOr },
+    { "=", Token::Assign },
+};
+};
