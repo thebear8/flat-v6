thebear8/flat-v6

commit 7ef56c2a6c7f4a20d320216bc587905208758561

Fix call target resolution breaking in case of recursive generic functions - `inferTypeArgsAndMatch()` did not add type args to `typeArgs` map correctly if type param and arg are equal - `findMatchingFunctions` using the wrong environment for constraint checking

flat-v6/passes/support/call_target_resolver.cpp
@@ -26,10 +26,7 @@ CallTargetResolver::matchFunction(
     auto zippedArgs = zip(function->params | std::views::values, args);
     for (auto [param, arg] : zippedArgs)
     {
-        if (param != arg
-            && !Environment::inferTypeArgsAndMatch(
-                param, arg, typeArgMap, true
-            ))
+        if (!Environment::inferTypeArgsAndMatch(param, arg, typeArgMap, true))
             return { std::nullopt, function };
     }
 
@@ -132,10 +129,11 @@ std::vector<IRFunction*> CallTargetResolver::findMatchingFunctions(
     optional_ref<std::set<IRFunction*>> requirementRejected
 )
 {
+    Environment* currentEnv = env;
     std::vector<std::pair<std::vector<IRType*>, IRFunction*>> candidates;
-    while (env != nullptr)
+    while (currentEnv != nullptr)
     {
-        auto [it, end] = env->getFunctionMap().equal_range(name);
+        auto [it, end] = currentEnv->getFunctionMap().equal_range(name);
         std::ranges::for_each(
             std::ranges::subrange(it, end) | std::views::values
                 | std::views::filter([&](IRFunction* f) {
@@ -163,33 +161,36 @@ std::vector<IRFunction*> CallTargetResolver::findMatchingFunctions(
             candidates.push_back(f);
             });
 
-        env = env->getParent();
+        currentEnv = currentEnv->getParent();
     }
 
     std::ranges::sort(candidates, [](auto const& a, auto const& b) {
         return a.first.size() > b.first.size();
     });
 
-    return candidates | std::views::transform([&](auto const& f) {
-               auto function = f.second;
-               auto functionInstantiation =
-                   m_instantiator.getFunctionInstantiation(function, f.first);
+    auto functions =
+        candidates | std::views::transform([&](auto const& f) {
+            auto function = f.second;
+            auto functionInstantiation =
+                m_instantiator.getFunctionInstantiation(function, f.first);
 
-               // We have to add the function instantiation to the parent env
-               // here, as we
-               // only now know that the instantiation is legal.
-               // If we do so in the instantiator, we are going to get errors
-               // later
-               if (functionInstantiation != function)
-               {
-                   function->parent->getEnv()->addFunctionInstantiation(
-                       function, functionInstantiation
-                   );
-               }
+            // We have to add the function instantiation to the parent env
+            // here, as we
+            // only now know that the instantiation is legal.
+            // If we do so in the instantiator, we are going to get errors
+            // later
+            if (functionInstantiation != function)
+            {
+                function->parent->getEnv()->addFunctionInstantiation(
+                    function, functionInstantiation
+                );
+            }
 
-               return functionInstantiation;
-           })
+            return functionInstantiation;
+        })
         | range_utils::to_vector;
+
+    return functions;
 }
 
 bool CallTargetResolver::isConstraintSatisfied(

flat-v6/environment.cpp
@@ -347,11 +347,8 @@ bool Environment::inferTypeArgsAndMatch(
     optional_ref<std::string> reason
 )
 {
-    if (actualType == genericType)
-    {
-        return true;
-    }
-    else if (genericType->isGenericType() && (!actualType->isGenericType() || allowGenericSubstitution))
+    if (genericType->isGenericType()
+        && (!actualType->isGenericType() || allowGenericSubstitution))
     {
         if (!typeArgs.contains((IRGenericType*)genericType))
             typeArgs.try_emplace((IRGenericType*)genericType, actualType);
@@ -369,6 +366,10 @@ bool Environment::inferTypeArgsAndMatch(
             return false;
         }
 
+        return true;
+    }
+    else if (actualType == genericType)
+    {
         return true;
     }
     else if (genericType->isStructInstantiation())

commit d38e0270fea4c614b3e5ecfa1bae1ebea8e957f0

Fix bug introduced with commit https://github.com/thebear8/flat-v6/commit/7ef56c2a6c7f4a20d320216bc587905208758561

flat-v6/passes/support/call_target_resolver.cpp
@@ -129,11 +129,10 @@ std::vector<IRFunction*> CallTargetResolver::findMatchingFunctions(
     optional_ref<std::set<IRFunction*>> requirementRejected
 )
 {
-    Environment* currentEnv = env;
     std::vector<std::pair<std::vector<IRType*>, IRFunction*>> candidates;
-    while (currentEnv != nullptr)
+    while (env != nullptr)
     {
-        auto [it, end] = currentEnv->getFunctionMap().equal_range(name);
+        auto [it, end] = env->getFunctionMap().equal_range(name);
         std::ranges::for_each(
             std::ranges::subrange(it, end) | std::views::values
                 | std::views::filter([&](IRFunction* f) {
@@ -161,7 +160,7 @@ std::vector<IRFunction*> CallTargetResolver::findMatchingFunctions(
             candidates.push_back(f);
             });
 
-        currentEnv = currentEnv->getParent();
+        env = env->getParent();
     }
 
     std::ranges::sort(candidates, [](auto const& a, auto const& b) {